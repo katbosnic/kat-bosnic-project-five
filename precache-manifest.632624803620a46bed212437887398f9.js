59907b91062d5b53a804849b6c6bd57c3bbce3e1
katbosnic <katbosnic@gmail.com>
2019-06-03 11:14:51 -0400

    Updates

---
+++ precache-manifest.632624803620a46bed212437887398f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badfbb6813d7ccc19890fc02484f7838",
+    "url": "%20//katbosnic.github.io/kat-bosnic-project-five/index.html"
+  },
+  {
+    "revision": "44a1e37d1f5356f5b5d0",
+    "url": "%20//katbosnic.github.io/kat-bosnic-project-five/static/css/main.c6dd8c51.chunk.css"
+  },
+  {
+    "revision": "d607a2072eaa49599e52",
+    "url": "%20//katbosnic.github.io/kat-bosnic-project-five/static/js/2.c4d74522.chunk.js"
+  },
+  {
+    "revision": "44a1e37d1f5356f5b5d0",
+    "url": "%20//katbosnic.github.io/kat-bosnic-project-five/static/js/main.418bb03e.chunk.js"
+  },
+  {
+    "revision": "515ee63f45f4dcf6d74b",
+    "url": "%20//katbosnic.github.io/kat-bosnic-project-five/static/js/runtime~main.771685cc.js"
+  }
+]);
